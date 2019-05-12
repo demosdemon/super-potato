e837464ed31fd36092f074f2c4b5fc8fbf3011ab
Brandon LeBlanc <brandon@platform.sh>
2019-05-12 12:24:22 -0500

    reorganize mounts

--- .platform/build.sh
+++ .platform/build.sh
@@ -2,12 +2,6 @@
 
 set -eux
 time go get ./...
-time go build -v -a -ldflags '-s -w -extldflags "-static"' ./cmd/gen
-# fail to build if gen writes a file
-time ./gen --exit-code enums ./data/enums.yaml ./pkg/platformsh/enums_gen.go
-time ./gen --exit-code variables ./data/variables.yaml ./pkg/platformsh/environment_gen.go
-time ./gen --exit-code api ./data/variables.yaml ./cmd/serve/generated.go
+time go generate ./...
 time go build -v -a -ldflags '-s -w -extldflags "-static"' .
-time ./super-potato dump -f environ > data/.build_env
-time ./super-potato dump -f shell > data/build_env.sh
-ls -l super-potato gen
+ls -l super-potato
--- .platform/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-time ./super-potato dump -f environ > tmp/.deploy_env
-time ./super-potato dump -f shell > tmp/deploy_env.sh
