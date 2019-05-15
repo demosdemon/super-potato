38b463dcd279538401c147a14361287c9eb50522
Brandon LeBlanc <brandon@platform.sh>
2019-05-15 17:39:24 -0500

    refactor root command

--- main.go
+++ main.go
@@ -5,40 +5,57 @@ package main
 import (
 	"context"
 
-	"github.com/demosdemon/super-potato/cmd/secret"
-
+	"github.com/octago/sflags/gen/gpflag"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
 	"github.com/demosdemon/super-potato/cmd/dump"
 	"github.com/demosdemon/super-potato/cmd/scrape"
+	"github.com/demosdemon/super-potato/cmd/secret"
 	"github.com/demosdemon/super-potato/cmd/serve"
 	"github.com/demosdemon/super-potato/pkg/app"
 )
 
+type Config struct {
+	*app.App  `flag:"-"`
+	LogLevel  string `flag:"log-level l" desc:"The logging verbosity"`
+	LogOutput string `flag:"log-output" desc:"Where logging is written"`
+}
+
+func (c *Config) Run(cmd *cobra.Command, args []string) error {
+	level, err := logrus.ParseLevel(c.LogLevel)
+	if err != nil {
+		return err
+	}
+
+	fp, err := c.GetOutput(c.LogOutput)
+	if err != nil {
+		return err
+	}
+
+	logrus.SetLevel(level)
+	logrus.SetOutput(fp)
+	return nil
+}
+
 func Command(app *app.App) *cobra.Command {
 	logrus.SetOutput(app.Stderr)
 
-	rv := cobra.Command{
-		Use: "super-potato",
-		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-			ll, err := cmd.Flags().GetString("log-level")
-			if err != nil {
-				return err
-			}
-
-			level, err := logrus.ParseLevel(ll)
-			if err != nil {
-				return err
-			}
-
-			logrus.SetLevel(level)
-			return nil
-		},
+	cfg := Config{
+		App:       app,
+		LogLevel:  "trace",
+		LogOutput: "/dev/stderr",
 	}
 
-	flags := rv.PersistentFlags()
-	flags.StringP("log-level", "l", "trace", "control the logging verbosity")
+	rv := cobra.Command{
+		Use:               "super-potato",
+		PersistentPreRunE: cfg.Run,
+	}
+
+	err := gpflag.ParseTo(&cfg, rv.PersistentFlags())
+	if err != nil {
+		logrus.WithField("err", err).Fatal("failed to parse config flags")
+	}
 
 	rv.AddCommand(dump.Command(app))
 	rv.AddCommand(serve.Command(app))
