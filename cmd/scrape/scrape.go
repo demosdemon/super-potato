1c84acc6184f4b7c84da604bdeb84fc695e433f0
Brandon LeBlanc <brandon@platform.sh>
2019-05-15 17:32:20 -0500

    refactor scrape command

--- cmd/scrape/scrape.go
+++ cmd/scrape/scrape.go
@@ -7,44 +7,71 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/octago/sflags/gen/gpflag"
+	"github.com/sirupsen/logrus"
+
 	"github.com/spf13/cobra"
 
 	"github.com/demosdemon/super-potato/pkg/app"
 )
 
+type Config struct {
+	*app.App `flag:"-"`
+	Output   string `flag:"output o" desc:"Where the output is written"`
+}
+
+func (c *Config) Run(cmd *cobra.Command, args []string) error {
+	rootURL := args[0]
+
+	resp, err := http.Get(rootURL)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return NewResponseError(resp)
+	}
+
+	page := NewCharacterPage(resp.Body)
+	ctx, cancel := context.WithTimeout(c, time.Minute)
+	list, err := page.Collect(ctx)
+	cancel()
+
+	fp, err := c.GetOutput(c.Output)
+	if err != nil {
+		return err
+	}
+
+	defer fp.Close()
+
+	m := make(map[string]struct{})
+	for _, v := range list {
+		if _, ok := m[v.Name]; !ok {
+			fmt.Fprintln(fp, v.Name)
+		}
+		m[v.Name] = struct{}{}
+	}
+
+	return nil
+}
+
 func Command(app *app.App) *cobra.Command {
+	cfg := Config{
+		App:    app,
+		Output: "-",
+	}
+
 	rv := cobra.Command{
 		Use:  "scrape URL",
 		Args: cobra.ExactArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			rootURL := args[0]
+		RunE: cfg.Run,
+	}
 
-			resp, err := http.Get(rootURL)
-			if err != nil {
-				return err
-			}
-
-			defer resp.Body.Close()
-
-			if resp.StatusCode < 200 || resp.StatusCode >= 300 {
-				return NewResponseError(resp)
-			}
-
-			page := NewCharacterPage(resp.Body)
-			ctx, cancel := context.WithTimeout(app, time.Minute)
-			list, err := page.Collect(ctx)
-			cancel()
-
-			m := make(map[string]struct{})
-			for _, v := range list {
-				if _, ok := m[v.Name]; !ok {
-					fmt.Fprintln(app.Stdout, v.Name)
-				}
-				m[v.Name] = struct{}{}
-			}
-
-			return nil
-		},
+	err := gpflag.ParseTo(&cfg, rv.Flags())
+	if err != nil {
+		logrus.WithField("err", err).Fatal("failed to parse config flags")
 	}
 
 	return &rv
